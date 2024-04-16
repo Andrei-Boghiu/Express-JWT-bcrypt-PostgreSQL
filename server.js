133262496ba97d15d6f2e66c4c67c38b4a14ad62
Andrei-Boghiu <vandrei1408@yahoo.com>
2024-04-16 17:08:21 +0300

    work in progress...

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const app = express();
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const cookieParser = require('cookie-parser');
+const verifyJWT = require('./middleware/verifyJWT');
+const credentials = require('./middleware/credentials');
+
+app.use(credentials);
 
 app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
+
+app.use(verifyJWT); // Above will be unprotected routes, below protected routes
 
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
