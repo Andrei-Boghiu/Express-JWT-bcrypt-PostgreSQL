16f6a135dc4deb2363fa66669b9063a97e8bc224
Andrei-Boghiu <vandrei1408@yahoo.com>
2024-05-12 22:27:39 +0300

    Modified the authority of manager and team admin roles, and added comments for each role

--- models/roles.sql
+++ models/roles.sql
@@ -2,13 +2,13 @@ CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
     name VARCHAR(25) NOT NULL,
     authority_level INT DEFAULT 6
-)
+);
 
 INSERT INTO roles (id, name, authority_level)
-VALUES 
-    (1809, 'user', 5),
-    (1856, 'allocator', 4), 
-    (1911, 'team_admin', 3), 
-    (1945, 'manager', 2), 
-    (1969, 'admin', 1),
-    (1971, 'dev', 0)
+VALUES
+    (1809, 'user', 5), -- Will have access to process work items
+    (1856, 'allocator', 4), -- previous role privileges + will have access to insert work items and view team statistics
+    (1945, 'manager', 3), -- will have visibility over the team statistics
+    (1911, 'team_admin', 2), -- allocator and manager privileges + can create teams (needs approval) and view teams statistics
+    (1969, 'admin', 1), -- can approve or deny new teams and manage every team and user
+    (1971, 'dev', 0); -- same as admin but will have access to a thread with reported issues and feature requests
