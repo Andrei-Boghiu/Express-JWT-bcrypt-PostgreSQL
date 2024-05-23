ec5471402bd34edb3823c6fd4ccbc40c8252cfd2
Andrei-Boghiu <vandrei1408@yahoo.com>
2024-05-23 19:15:21 +0300

    Work items ingestion management

--- controllers/work/insertWorkItems.js
+++ controllers/work/insertWorkItems.js
@@ -3,6 +3,13 @@ const pool = require('../../config/db');
 module.exports = insertWorkItems = async (req, res) => {
     try {
         const workItems = req.body;
+        const approverId = req.user.id;
+        const teamId = req.headers?.team_id;
+
+        console.log("teamId:", teamId);
+        console.log("approverId:", approverId);
+
+        // console.log(workItems)
         const client = await pool.connect();
         try {
             await client.query('BEGIN');
@@ -11,15 +18,19 @@ module.exports = insertWorkItems = async (req, res) => {
                 const keys = Object.keys(item);
                 const columns = keys.toString().replaceAll(',', ', ');
                 const valuesArr = Object.values(item);
+                valuesArr.push(teamId)
 
                 let wildcards = '';
                 keys.forEach((_, index) => {
                     wildcards = wildcards + `$${index + 1}${index === keys.length - 1 ? '' : ', '}`;
                 });
 
-                const query = `INSERT INTO work_items (${columns}) VALUES(${wildcards})`;
+                const query = `INSERT INTO work_items (${columns}, team_id) VALUES(${wildcards})`;
+                console.log(query);
+                console.log(valuesArr);
 
-                await client.query(query, valuesArr);
+                break
+                // await client.query(query, valuesArr);
             }
 
             await client.query('COMMIT');
